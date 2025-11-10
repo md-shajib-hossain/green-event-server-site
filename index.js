133f53fdf5ffe8d6cc6020acbeb096841b5bc9a8
unknown <mdshajib5159@gmail.com>
2025-11-10 15:52:42 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
-const a = 5;
-const b = 10;
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const c = 15;
+// middleware
+app.use(cors());
+app.use(express.json());
+//
+app.get("/", (req, res) => {
+  res.send("helllo brother");
+});
+
+// mongodb
+// greendb
+// fF6KPdEF9fXCKbvu
+
+const uri =
+  "mongodb+srv://greendb:fF6KPdEF9fXCKbvu@cluster0.xg4rh8d.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    //
+    const db = client.db("events_db");
+    const eventsCollection = db.collection("events");
+
+    //
+    app.get("/events", async (req, res) => {
+      const result = await eventsCollection.find().limit(2).toArray();
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
